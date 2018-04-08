0b61b81ea3aed9710e3f2c706d73140ffb5e7af8
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-04-09 00:41:43 +0100

    Fix greedy algorithm

--- dense-linear-operator/greedy.hs
+++ dense-linear-operator/greedy.hs
@@ -29,23 +29,25 @@ inputs n = Set.fromList $ map (\x -> (x, x)) [0..(n - 1)]
 -- Add a new node to the circuit by concatenating two existing nodes, hitting
 -- as many nodes of the goal as possible. Returns Nothing if the goal has
 -- already been reached.
-greedyStep :: Goal -> Circuit -> Maybe (Int, Node)
+greedyStep :: Goal -> Circuit -> Maybe ((Bool, Int), Node)
 greedyStep goal circuit
-    | goal `Set.isSubsetOf` circuit = Nothing
+    | goal `Set.isSubsetOf` circuit = Nothing -- Done: No more nodes are needed
     | otherwise = Just $ maximum nodes
   where
-    nodes = [ (length $ filter (subNode c) (Set.toList goal), c)
+    nodes = [ (score c $ filter (subNode c) (Set.toList goal), c)
             | x <- Set.elems circuit, y <- Set.elems circuit, snd x + 1 == fst y
             , let c = (fst x, snd y), c `Set.notMember` circuit ]
-    subNode (x1, x2) (y1, y2) = x1 >= y1 && x2 <= y2
+    subNode (x1, x2) (y1, y2) = x1 >= y1 && x2 <= y2 && (x1 == y1 || x2 == y2)
+    score n ns = (n `Set.member` goal, length ns) -- Prefer completing goals
 
 greedy :: Goal -> Circuit -> IO [Node]
 greedy goal circuit = case greedyStep goal circuit of
     Nothing -> return []
     Just (gain, node) -> do
         putStrLn $ "Add node " ++ show node ++ " with gain = " ++ show gain
-        (node :) <$> greedy goal (Set.insert node circuit)
+        (node :) <$> greedy (Set.delete node goal) (Set.insert node circuit)
 
+-- Result = 8
 diagonalZeroes6x6 :: [[Int]]
 diagonalZeroes6x6 = [[0,1,1,1,1,1],
                      [1,0,1,1,1,1],
@@ -54,6 +56,7 @@ diagonalZeroes6x6 = [[0,1,1,1,1,1],
                      [1,1,1,1,0,1],
                      [1,1,1,1,1,0]]
 
+-- Result = 7
 custom6x6 :: [[Int]]
 custom6x6 = [[1,0,1,1,1,0],
              [1,1,1,1,0,1],
@@ -65,5 +68,6 @@ custom6x6 = [[1,0,1,1,1,0],
 main :: IO ()
 main = do
     let test = custom6x6
+    putStrLn $ "Goal = " ++ show (Set.toList $ toGoal test)
     res <- greedy (toGoal test) (inputs $ length test)
     putStrLn $ "Total nodes = " ++ show (length res)
